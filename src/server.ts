217b9ea08cfdffc66aa73fc89ad76aa8c43138ba
Raphael Freitas <raphael.silva@midway.com.br>
2020-10-15 19:56:48 -0300

    create database and setup orm

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import express from 'express'
+import './database/connection'
 
 const app = express()
+
+app.use(express.json())
+
+app.get('/users', (req, res) => {
+  return res.json({ message: 'Olá Mundo' })
+})
 
 app.listen(3333)
